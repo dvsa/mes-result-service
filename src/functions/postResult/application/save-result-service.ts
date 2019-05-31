a38ee227b5cb09c4cf609781fdebe163bbad3905
John Morley <john.morley@bjss.com>
2019-05-31 11:33:20 +0100

    First attempt at decompression and saving

--- src/functions/postResult/application/save-result-service.ts
+++ src/functions/postResult/application/save-result-service.ts
@@ -1,11 +1,11 @@
 import { StandardCarTestCATBSchema } from '@dvsa/mes-test-schema/categories/B';
 import * as mysql from 'mysql2';
 import { config } from '../framework/config/config';
-import { query } from '../../../common/framework/mysql/database';
 
 export const saveTestResult = async (testResult: StandardCarTestCATBSchema): Promise<void> => {
+  console.log(`I will save ${JSON.stringify(testResult)}`);
   const configuration = config();
-  const connection = mysql.createConnection({
+  const connection: mysql.Connection = mysql.createConnection({
     host: configuration.mesDatabaseHostname,
     database: configuration.mesDatabaseName,
     user: configuration.mesDatabaseUsername,
@@ -19,8 +19,62 @@ export const saveTestResult = async (testResult: StandardCarTestCATBSchema): Pro
     },
   });
 
-  console.log('running insert...');
-  await query(connection, 'INSERT INTO TSTTBL VALUES(\'a\', \'b\')');
-  console.log('completed insert!');
-  return Promise.resolve();
+  const testResultInsert = buildResultInsertQuery(testResult);
+  console.log(`built query ${testResultInsert}`);
+
+  connection.beginTransaction((err) => {
+    if (err) {
+      throw err;
+    }
+    connection.query(testResultInsert, (err) => {
+      if (err) {
+        return connection.rollback(() => { throw err; });
+      }
+      connection.commit((err) => {
+        if (err) {
+          return connection.rollback(() => {
+            throw err;
+          });
+        }
+      });
+    });
+  });
+};
+
+const buildResultInsertQuery = (test: StandardCarTestCATBSchema): string => {
+  const template = `
+  INSERT INTO TEST_RESULT (
+    application_reference,
+    staff_number,
+    test_result,
+    test_date,
+    tc_id,
+    driver_number,
+    driver_surname,
+    result_status
+  )
+  VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+
+  const { applicationReference } = test.journalData;
+  const { staffNumber } = test.journalData.examiner;
+  const testResult = JSON.stringify(test);
+  const testDate = Date.parse(test.journalData.testSlotAttributes.start);
+  const testCentreId = 1; // TODO: Get this into the test schema
+  const { driverNumber } = test.journalData.candidate;
+  const driverSurname = test.journalData.candidate.candidateName.lastName;
+  const resultStatus = 0; // ACCEPTED;
+
+  const args = [
+    applicationReference,
+    staffNumber,
+    testResult,
+    testDate,
+    testCentreId,
+    driverNumber,
+    driverSurname,
+    resultStatus,
+  ];
+
+  return mysql.format(template, args);
 };
