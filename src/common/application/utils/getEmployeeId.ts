19e306a8373ca3cc429c93baa89c70f7cea79c11
John Morley <john.morley@bjss.com>
2019-07-09 13:27:26 +0100

    Fix incorrect logging when request context has no staff number

--- src/common/application/utils/getEmployeeId.ts
+++ src/common/application/utils/getEmployeeId.ts
@@ -3,8 +3,8 @@ import { APIGatewayEventRequestContext } from 'aws-lambda';
 
 export const getEmployeeIdFromRequestContext = (requestContext: APIGatewayEventRequestContext): string | null => {
   if (requestContext.authorizer && typeof requestContext.authorizer.staffNumber === 'string') {
-    logger.error('No staff number found in request context');
     return requestContext.authorizer.staffNumber;
   }
+  logger.error('No staff number found in request context');
   return null;
 };
