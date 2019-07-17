f9ef765f21c90860c2901b87f7d182b3329e43bc
John Morley <john.morley@bjss.com>
2019-07-17 14:19:40 +0100

    Run formatter on RetryProcessor spec file

--- src/functions/retryProcessing/application/__tests__/RetryProcessor.spec.ts
+++ src/functions/retryProcessing/application/__tests__/RetryProcessor.spec.ts
@@ -6,7 +6,7 @@ describe('retryProcessor database test', async () => {
     console.log('database initialised');
   });
 
-// before all to ensure only run once per describe
+  // before all to ensure only run once per describe
 
   it('should run processSuccessful and check TEST_RESULT has a status of 2 (PROCESSED)', async () => {
     await retryProcessor.processSuccessful();
@@ -38,100 +38,114 @@ describe('retryProcessor database test', async () => {
 
 const checkProcessSuccessfulUpdatedTestResult = (): Promise<void> => {
   return new Promise((resolve, reject) => {
-    db.get(`SELECT * FROM TEST_RESULT WHERE application_reference = 1
-    and staff_number = '1234' and result_status = 2;`,
-           [], (err, row) => {
-             if (err || !row) {
-               reject('Row not found or incorrect state');
-             } else {
-               resolve();
-             }
-           });
+    db.get(
+      `
+      SELECT * FROM TEST_RESULT WHERE application_reference = 1
+      and staff_number = '1234' and result_status = 2
+      `,
+      [],
+      (err, row) => {
+        if (err || !row) {
+          reject('Row not found or incorrect state');
+        }
+        resolve();
+      });
   });
 };
 
 const checkErrorsToRetryUpdatedUpLoadQueues = (): Promise<void> => {
   return new Promise((resolve, reject) => {
-    db.get(`SELECT COUNT(*) as rowcount FROM UPLOAD_QUEUE WHERE application_reference = 2
-    and staff_number = '1234' and upload_status = 0;`,
-           [], (err, row) => {
-             if (err || !row) {
-               reject('Row not found or incorrect state');
-             } else {
-               if (row.rowcount && row.rowcount !== 3) {
-                 reject('Row count does not match expected (3)');
-               } else {
-                 resolve();
-               }
-             }
-           });
+    db.get(
+      `
+      SELECT COUNT(*) as rowcount FROM UPLOAD_QUEUE WHERE application_reference = 2
+      and staff_number = '1234' and upload_status = 0
+      `,
+      [],
+      (err, row) => {
+        if (err || !row) {
+          reject('Row not found or incorrect state');
+        }
+        if (row.rowcount && row.rowcount !== 3) {
+          reject('Row count does not match expected (3)');
+        }
+        resolve();
+      });
   });
 };
 
 const checkErrorsToAbortUpdatedTestResult = (): Promise<void> => {
   return new Promise((resolve, reject) => {
-    db.get(`SELECT *  FROM TEST_RESULT WHERE application_reference = 3
-    and staff_number = '1234'`,
-           [], (err, row) => {
-             if (err || !row) {
-               reject('Row not found or incorrect state');
-             } else {
-               if (row.result_status && row.result_status !== 4) {
-                 reject('result status Row count does not match expected (4)');
-               } else {
-                 resolve();
-               }
-             }
-           });
+    db.get(
+      `
+      SELECT *  FROM TEST_RESULT WHERE application_reference = 3
+      and staff_number = '1234'
+      `,
+      [],
+      (err, row) => {
+        if (err || !row) {
+          reject('Row not found or incorrect state');
+        }
+        if (row.result_status && row.result_status !== 4) {
+          reject('result status Row count does not match expected (4)');
+        }
+        resolve();
+      });
   });
 };
 
 const checkSupportInterventionUpdatedUploadQueues = (): Promise<void> => {
   return new Promise((resolve, reject) => {
-    db.get(`SELECT COUNT(*) as rowcount FROM UPLOAD_QUEUE WHERE application_reference = 4
-    and staff_number = '1234' and upload_status = 0;`,
-           [], (err, row) => {
-             if (err || !row) {
-               reject('Row not found or incorrect state');
-             } else {
-               if (row.rowcount && row.rowcount !== 3) {
-                 reject('Row count does not match expected (3)');
-               } else {
-                 resolve();
-               }
-             }
-           });
+    db.get(
+      `
+      SELECT COUNT(*) as rowcount FROM UPLOAD_QUEUE WHERE application_reference = 4
+      and staff_number = '1234' and upload_status = 0
+      `,
+      [],
+      (err, row) => {
+        if (err || !row) {
+          reject('Row not found or incorrect state');
+        }
+        if (row.rowcount && row.rowcount !== 3) {
+          reject('Row count does not match expected (3)');
+        }
+        resolve();
+      });
   });
 };
 
 const checkSupportInterventionUpdatedTestResult = (): Promise<void> => {
   return new Promise((resolve, reject) => {
-    db.get(`SELECT * FROM TEST_RESULT WHERE application_reference = 4
-    and staff_number = '1234' and result_status = 1;`,
-           [], (err, row) => {
-             if (err || !row) {
-               reject('Row not found or incorrect state');
-             } else {
-               resolve();
-             }
-           });
+    db.get(
+      `
+      SELECT * FROM TEST_RESULT WHERE application_reference = 4
+      and staff_number = '1234' and result_status = 1
+      `,
+      [],
+      (err, row) => {
+        if (err || !row) {
+          reject('Row not found or incorrect state');
+        }
+        resolve();
+      });
   });
 };
 
 const checkOldEntryCleanupDeleteUploadQueues = (): Promise<void> => {
   return new Promise((resolve, reject) => {
-    db.get(`SELECT COUNT(*) as rowcount FROM UPLOAD_QUEUE WHERE application_reference = 5
-    and staff_number = '1234';`,
-           [], (err, row) => {
-             if (err || !row) {
-               reject('Row not found or incorrect state');
-             } else {
-               if (row.rowcount && row.rowcount !== 0) {
-                 reject('Row count does not match expected (0)');
-               } else {
-                 resolve();
-               }
-             }
-           });
+    db.get(
+      `
+      SELECT COUNT(*) as rowcount FROM UPLOAD_QUEUE WHERE application_reference = 5
+      and staff_number = '1234'
+      `,
+      [],
+      (err, row) => {
+        if (err || !row) {
+          reject('Row not found or incorrect state');
+        }
+        if (row.rowcount && row.rowcount !== 0) {
+          reject('Row count does not match expected (0)');
+        }
+        resolve();
+      });
   });
 };
