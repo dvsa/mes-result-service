ec56c034aecd66dba061147e179d5f32f3f52196
John Morley <john.morley@bjss.com>
2019-07-17 13:24:39 +0100

    Use microservice common logging to log uncaught errors in handler

--- src/functions/retryProcessing/framework/handler.ts
+++ src/functions/retryProcessing/framework/handler.ts
@@ -4,15 +4,17 @@ import { HttpStatus } from '../../../common/application/api/HttpStatus';
 import Response from '../../../common/application/api/Response';
 import { RetryProcessor } from '../application/processRetries';
 import { bootstrapConfig } from '../../../../src/common/framework/config/config';
+import { IRetryProcessor } from '../application/IRetryProcessor';
+import { error } from '@dvsa/mes-microservice-common/application/utils/logger';
 
 export async function handler(event: APIGatewayEvent, fnCtx: Context): Promise<Response> {
-
   await bootstrapConfig();
-  const retryProcessor = new RetryProcessor();
+  const retryProcessor: IRetryProcessor = new RetryProcessor();
   try {
     await retryProcessor.processRetries();
   } catch (err) {
-    return createResponse(err, HttpStatus.BAD_REQUEST);
+    error('Uncaught error in handler', err);
+    return createResponse(err, HttpStatus.INTERNAL_SERVER_ERROR);
   }
   return createResponse({}, HttpStatus.OK);
 }
