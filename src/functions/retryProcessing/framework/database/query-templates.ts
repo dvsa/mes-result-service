9de60d938e62f658df3e2205734547836dac4863
John Morley <john.morley@bjss.com>
2019-07-19 09:57:18 +0100

    Reduce indentation on optimised queries

--- src/functions/retryProcessing/framework/database/query-templates.ts
+++ src/functions/retryProcessing/framework/database/query-templates.ts
@@ -1,37 +1,37 @@
 
 export const successfullyProcessedQuery = `
-    SELECT DISTINCT
-        uq.application_reference,
-        uq.staff_number
-    FROM UPLOAD_QUEUE uq
-    JOIN TEST_RESULT tr
-        ON uq.application_reference = tr.application_reference
-        AND uq.staff_number = tr.staff_number
-        AND tr.result_status = (SELECT id FROM RESULT_STATUS WHERE result_status_name = 'PROCESSING')
-    WHERE
-        (uq.application_reference, uq.staff_number) NOT IN (
-            SELECT application_reference, staff_number
-            FROM UPLOAD_QUEUE
-            WHERE upload_status != (SELECT id FROM PROCESSING_STATUS WHERE processing_status_name = 'ACCEPTED')
-        )
+  SELECT DISTINCT
+    uq.application_reference,
+    uq.staff_number
+  FROM UPLOAD_QUEUE uq
+  JOIN TEST_RESULT tr
+    ON uq.application_reference = tr.application_reference
+    AND uq.staff_number = tr.staff_number
+    AND tr.result_status = (SELECT id FROM RESULT_STATUS WHERE result_status_name = 'PROCESSING')
+  WHERE
+    (uq.application_reference, uq.staff_number) NOT IN (
+      SELECT application_reference, staff_number
+      FROM UPLOAD_QUEUE
+      WHERE upload_status != (SELECT id FROM PROCESSING_STATUS WHERE processing_status_name = 'ACCEPTED')
+    )
 `;
 
 export const errorsToRetryQueryTemplate = `
-    SELECT uq.application_reference, uq.staff_number, uq.interface
-    FROM UPLOAD_QUEUE uq
-    JOIN TEST_RESULT tr
-        ON uq.application_reference = tr.application_reference
-        AND uq.staff_number = tr.staff_number
-        AND tr.result_status = (SELECT id FROM RESULT_STATUS WHERE result_status_name = 'PROCESSING')
-    WHERE
-        uq.upload_status = (SELECT id FROM PROCESSING_STATUS WHERE processing_status_name = 'FAILED')
-        AND (
-            (uq.interface = (SELECT id FROM INTERFACE_TYPE WHERE interface_type_name = 'RSIS') AND uq.retry_count < ?)
-            OR
-            (uq.interface = (SELECT id FROM INTERFACE_TYPE WHERE interface_type_name = 'NOTIFY') AND uq.retry_count < ?)
-            OR
-            (uq.interface = (SELECT id FROM INTERFACE_TYPE WHERE interface_type_name = 'TARS') AND uq.retry_count < ?)
-        )
+  SELECT uq.application_reference, uq.staff_number, uq.interface
+  FROM UPLOAD_QUEUE uq
+  JOIN TEST_RESULT tr
+    ON uq.application_reference = tr.application_reference
+    AND uq.staff_number = tr.staff_number
+    AND tr.result_status = (SELECT id FROM RESULT_STATUS WHERE result_status_name = 'PROCESSING')
+  WHERE
+    uq.upload_status = (SELECT id FROM PROCESSING_STATUS WHERE processing_status_name = 'FAILED')
+    AND (
+      (uq.interface = (SELECT id FROM INTERFACE_TYPE WHERE interface_type_name = 'RSIS') AND uq.retry_count < ?)
+      OR
+      (uq.interface = (SELECT id FROM INTERFACE_TYPE WHERE interface_type_name = 'NOTIFY') AND uq.retry_count < ?)
+      OR
+      (uq.interface = (SELECT id FROM INTERFACE_TYPE WHERE interface_type_name = 'TARS') AND uq.retry_count < ?)
+    )
 `;
 
 export const errorsToAbortQueryTemplate = `
