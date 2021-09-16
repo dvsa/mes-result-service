8ceae100312ac7e6d637123f0777c0b9911e9139
kevin <kevin.richards@dvsa.gov.uk>
2021-09-16 16:35:47 +0100

    Debugging DEV error

--- src/functions/deleteTestResult/framework/database/query-builder.ts
+++ src/functions/deleteTestResult/framework/database/query-builder.ts
@@ -2,7 +2,7 @@ import * as mysql from 'mysql2';
 
 export const deleteTestResultRecord = (): string => {
   const template = `
-  DELETE FROM TEST_RESULT WHERE test_date <= DATE_ADD(NOW(), INTERVAL -2 YEAR)`;
+  SELECT COUNT(*) FROM TEST_RESULT WHERE test_date <= DATE_ADD(NOW(), INTERVAL -2 YEAR)`;
   const args = [];
 
   return mysql.format(template, args);
