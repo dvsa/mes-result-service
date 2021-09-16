74ec6b341fa143a01abbfde0d412e693bd6c78fb
kevin <kevin.richards@dvsa.gov.uk>
2021-09-16 16:45:35 +0100

    Reverting to previous code

--- src/functions/deleteTestResult/application/delete-test-result-service.ts
+++ src/functions/deleteTestResult/application/delete-test-result-service.ts
@@ -2,7 +2,7 @@ import * as mysql from 'mysql2';
 import { getConnection } from '../../../common/framework/mysql/database';
 import { NoDeleteWarning } from '../domain/NoDeleteWarning';
 import { info } from '@dvsa/mes-microservice-common/application/utils/logger';
-import { deleteTestResultRecord} from '../framework/database/query-builder';
+import { deleteTestResultRecord } from '../framework/database/query-builder';
 
 export const deleteTestResult = async (): Promise<void> => {
   const connection: mysql.Connection = getConnection();
