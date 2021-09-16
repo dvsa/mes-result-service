365eb38483cae667cfa5f690410ccb7b073f4a52
kevin <kevin.richards@dvsa.gov.uk>
2021-09-16 16:45:35 +0100

    Reverting to previous code

--- src/functions/deleteTestResult/framework/database/query-builder.ts
+++ src/functions/deleteTestResult/framework/database/query-builder.ts
@@ -2,7 +2,7 @@ import * as mysql from 'mysql2';
 
 export const deleteTestResultRecord = (): string => {
   const template = `
-  SELECT COUNT(*) FROM TEST_RESULT WHERE test_date <= DATE_ADD(NOW(), INTERVAL -2 YEAR)`;
+  DELETE FROM TEST_RESULT WHERE test_date <= DATE_ADD(NOW(), INTERVAL -2 YEAR)`;
   const args = [];
 
   return mysql.format(template, args);
