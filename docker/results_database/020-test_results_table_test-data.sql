a3bf09269261f35f269301c9c64226de0b68e637
andrewsetterfield <48550267+andrewsetterfield@users.noreply.github.com>
2023-03-22 15:57:25 +0000

    Local docker database (#286)
    * docker compose and sql scripts for local mysql db
    
    * updated README.md
    
    * update ignorefile

---
+++ docker/results_database/020-test_results_table_test-data.sql
@@ -0,0 +1,23 @@
+INSERT INTO TEST_RESULT(
+    application_reference,
+    staff_number,
+    test_result,
+    test_date,
+    tc_id,
+    tc_cc,
+    driver_number,
+    driver_surname,
+    result_status,
+    autosave,
+    activity_code,
+    category,
+    pass_certificate_number,
+    version,
+    app_version
+) VALUES
+    ('10000000000','55555555','{}','2022-10-01',1,1,'DNum1','Pearson',1,false,'1','B','D118101x','3.41.1','4.7.0.0')
+    ,('10000000001','55555555','{}','2022-10-01',1,1,'DNum1','Pearson',1,false,'1','B','D118102x','3.41.1','4.7.0.0')
+    ,('10000000002','55555555','{}','2022-10-01',1,1,'DNum1','Pearson',1,false,'1','B','D118104x','3.41.1','4.7.0.0')
+    ,('10000000003','55555555','{}','2022-10-01',1,1,'DNum1','Pearson',1,false,'1','B','D118105x','3.41.1','4.7.0.0')
+    ,('10000000004','55555555','{}','2022-10-01',1,1,'DNum1','Pearson',1,false,'1','B','D118106x','3.41.1','4.7.0.0')
+;
