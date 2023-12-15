5eac3494c2814ba780ad47de6329e175d0af679f
Matthew Bell <mbell.dvsa@icloud.com>
2023-12-15 10:09:55 +0000

    Update specs

--- src/functions/searchExaminerRecords/application/__tests__/validate-request.spec.ts
+++ src/functions/searchExaminerRecords/application/__tests__/validate-request.spec.ts
@@ -14,7 +14,19 @@ describe('Validate request', () => {
       expect(res.error).not.toBeNull();
     });
 
-    it('should return error if not meeting validation', () => {
+    it('should not require staffNumber for LDTM', () => {
+      const qp = new QueryParameters();
+
+      qp.role = ExaminerRole.LDTM;
+      qp.startDate = '2023-12-01';
+      qp.endDate = '2023-12-05';
+
+      const res = validateExaminerRecordsSchema(qp);
+
+      expect(res.error).toEqual(undefined);
+    });
+
+    it('should require staffNumber for DE', () => {
       const qp = new QueryParameters();
 
       qp.role = ExaminerRole.DE;
